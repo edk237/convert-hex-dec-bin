a7024ce986fe66e194699529c3045b4ba0d42583
Raphael VERDOIT <115892718+lunarsol@users.noreply.github.com>
2024-10-02 09:11:43 +0200

    Operational
    Can transform any base 10,16,2

--- cs_convert.py
+++ cs_convert.py
@@ -16,10 +16,11 @@ while source == translated:
     for i, value in enumerate(convert_opt): #Opiton's list
         print(i, '-', convert_opt[value])
     translated = int(input("Vers quel format traduire ? (0-2) :"))
-    print("                              ")
-    print("YOU CANT CONVERT THE SAME DATA")
-    print("                              ")
-    input("Press ENTER when you read...")
+    if source == translated:
+      print("                              ")
+      print("YOU CANT CONVERT THE SAME DATA")
+      print("                              ")
+      input("Press ENTER when you read...")
 
 
 if source == 0:     #If we Choosed Binary
@@ -43,36 +44,56 @@ elif source == 2:   #If we Choosed Hexa
     while not all(char.upper() in '0123456789ABCDEF' for char in value_source): #Verify Characters
         print("Veuillez entrer une valeur hexadécimale valide (0-9, A-F).")
         value_source = input("Enter an hexa value : ")
+        value_source = int(value_source, 16)
+        print(value_source)
 
+#Fonction Decimal to ?
 def dec_binary(d):     # Decimal to Binary
     print(bin(d))
-
 def dec_hex(d):     # Decimal to Hexa
-    print(hex(d))
-
-def hex_binary(h):     # Hexa to Binary
-    print(f'{0xdaf:b}')
-
+   print(hex(d))
+#Function Hexa to ?
+def hex_binary(h): # Hexa to Binary
+    h = int(value_source, 16)
+    str(h)
+    print(bin(h))
 def hex_dec(h):     # Hexa to Decimal
     h = int(value_source, 16)
     str(h)
     print(h)
-
-# def bin_dec(b):
-# def bin_hex(b):
-
+#Function Binary to ?
+def bin_dec(b):
+    b = int(value_source, 2)
+    str(b)
+    print(b)
+def bin_hex(b):
+    b = int(value_source, 2)
+    str(b)
+    print(hex(b))
+    
+#Bin -> Dec
+if source == 0 and translated == 1:
+  print(f"You're going to transform {value_source} into Decimal")
+  bin_dec(value_source)
+#Bin -> Hex
+if source == 0 and translated == 2:
+    print(f"You're going to transform {value_source} into Decimal")
+    bin_hex(value_source)
+    
+#Dec -> Bin
 if source == 1 and translated == 0:    # Decimal To Binary
     print(f"You're going to transform {value_source} into Binary")
     dec_binary(value_source)
-
-if source == 1 and translated == 2:    # Decimal To Hexa
+#Dec -> Hex
+if source == 1 and translated == 2:
     print(f"You're going to transform {value_source} into Hexa")
     dec_hex(value_source)
 
-if source == 2 and translated == 1:    # Hexa To Decimal
+#Hex -> Dec
+if source == 2 and translated == 1:
     print(f"You're going to transform {value_source} into Hexa")
     hex_dec(value_source)
-
-if source == 2 and translated == 0:    # Hexa To Binary
+#Hex -> Bin
+if source == 2 and translated == 0:
     print(f"You're going to transform {value_source} into Hexa")
     hex_binary(value_source)
